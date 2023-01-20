3cefd51b22b552adb3dd484a75035991124cdb36
James Sturtevant <jstur@microsoft.com>
2023-01-19 19:41:36 -0800

    clean up
    Signed-off-by: James Sturtevant <jstur@microsoft.com>

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,23 +1,16 @@
 use mio::windows::NamedPipe;
-use std::io::{Write, Read};
-use std::os::windows::fs::OpenOptionsExt;
 use std::fs::OpenOptions;
+use std::io::{Read, Write};
+use std::os::windows::fs::OpenOptionsExt;
 use std::os::windows::io::{FromRawHandle, IntoRawHandle};
 
-use windows_sys::Win32::Storage::FileSystem::{
-     FILE_FLAG_OVERLAPPED,
-     OPEN_EXISTING
-};
+use windows_sys::Win32::Storage::FileSystem::FILE_FLAG_OVERLAPPED;
 
-use windows_sys::Win32::Foundation::{
-    ERROR_BROKEN_PIPE, ERROR_PIPE_NOT_CONNECTED
-};
 use mio::{Events, Interest, Poll, Token};
 use std::error::Error;
-use std::time::Duration;
+use windows_sys::Win32::Foundation::ERROR_PIPE_NOT_CONNECTED;
 
-
-fn main() -> Result<(), Box<dyn Error>>{
+fn main() -> Result<(), Box<dyn Error>> {
     println!("Hello, client!");
 
     let mut opts = OpenOptions::new();
@@ -28,10 +21,9 @@ fn main() -> Result<(), Box<dyn Error>>{
     let mut client = unsafe { NamedPipe::from_raw_handle(file.into_raw_handle()) };
 
     let mut poll = Poll::new()?;
-    poll
-        .registry()
+    poll.registry()
         .register(&mut client, Token(1), Interest::WRITABLE)?;
-       
+
     let mut events = Events::with_capacity(128);
 
     // initiate ping/pong
@@ -43,50 +35,50 @@ fn main() -> Result<(), Box<dyn Error>>{
 
     let mut count = 0;
     'outer: loop {
-        poll.registry().reregister(&mut client, Token(1), Interest::READABLE)?;
+        poll.registry()
+            .reregister(&mut client, Token(1), Interest::READABLE)?;
         poll.poll(&mut events, None).unwrap();
 
         let mut buf = [0; 10];
         match client.read(&mut buf) {
-            Ok(0) =>
-            {
+            Ok(0) => {
                 println!("Read zero from pipe");
                 continue;
-            },
+            }
             Ok(_) => {
                 println!("Read from pipe: {:?}", std::str::from_utf8(&buf));
-            },
-            Err(e) if e.raw_os_error() == Some(ERROR_PIPE_NOT_CONNECTED as i32) =>
-            {
+            }
+            Err(e) if e.raw_os_error() == Some(ERROR_PIPE_NOT_CONNECTED as i32) => {
                 println!("no process: {}", e);
 
                 break 'outer;
-            },
+            }
             Err(e) => println!("Error reading from pipe: {}", e),
         }
 
         std::thread::sleep(std::time::Duration::from_secs(1));
-        poll.registry().reregister(&mut client, Token(1), Interest::WRITABLE).unwrap();
+        poll.registry()
+            .reregister(&mut client, Token(1), Interest::WRITABLE)
+            .unwrap();
         count += 1;
         loop {
-            let message = format!("ping-{value}", value=count);
+            let message = format!("ping-{value}", value = count);
             match client.write(message.as_bytes()) {
                 Ok(_) => {
                     println!("Wrote to pipe");
                     break;
-                },
+                }
                 Err(e) if e.kind() == std::io::ErrorKind::WouldBlock => {
                     poll.poll(&mut events, None).unwrap();
-                },
-                Err(e) if e.raw_os_error() == Some(ERROR_PIPE_NOT_CONNECTED as i32) =>
-                {
+                }
+                Err(e) if e.raw_os_error() == Some(ERROR_PIPE_NOT_CONNECTED as i32) => {
                     println!("no process: {}", e);
 
                     break 'outer;
-                },
+                }
                 Err(e) => {
                     println!("Error writing to pipe: {}", e);
-                } 
+                }
             }
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -4,35 +4,31 @@
 use mio::windows::NamedPipe;
 
 use mio::{Events, Interest, Poll, Token};
-use std::error::Error;
+
 use std::ffi::c_void;
-use std::io::{Read, Write, self};
-use std::time::Duration;
-use std::thread;
+use std::io::{self, Read, Write};
+
 use std::ffi::OsStr;
 use std::os::windows::ffi::OsStrExt;
+use std::os::windows::io::{FromRawHandle, RawHandle};
+use std::thread;
 use windows_sys::Win32::System::Pipes::{
-    ConnectNamedPipe, CreateNamedPipeW, DisconnectNamedPipe, PIPE_TYPE_BYTE,
-    PIPE_UNLIMITED_INSTANCES,
+    CreateNamedPipeW, PIPE_TYPE_BYTE, PIPE_UNLIMITED_INSTANCES,
 };
-use std::os::windows::io::{AsRawHandle, FromRawHandle, RawHandle};
 
+use windows_sys::Win32::Foundation::INVALID_HANDLE_VALUE;
 use windows_sys::Win32::Storage::FileSystem::{
-    ReadFile, WriteFile, FILE_FLAG_FIRST_PIPE_INSTANCE, FILE_FLAG_OVERLAPPED, PIPE_ACCESS_DUPLEX,
-};
-use windows_sys::Win32::Foundation::{
-    ERROR_BROKEN_PIPE, ERROR_IO_INCOMPLETE, ERROR_IO_PENDING, ERROR_NO_DATA, ERROR_PIPE_CONNECTED,
-    ERROR_PIPE_LISTENING, HANDLE, INVALID_HANDLE_VALUE,
+    FILE_FLAG_FIRST_PIPE_INSTANCE, FILE_FLAG_OVERLAPPED, PIPE_ACCESS_DUPLEX,
 };
 
 const PIPE_NAME: &str = r"\\.\pipe\mio-named-pipe-test";
 const SERVER: Token = Token(0);
 
-fn main()  {
+fn main() {
     println!("Hello, server!");
 
     let mut server = new(PIPE_NAME, true, std::ptr::null_mut()).unwrap();
-    
+
     loop {
         server = waitForConnection(server).unwrap();
     }
@@ -43,61 +39,63 @@ fn waitForConnection(server: NamedPipe) -> io::Result<NamedPipe> {
 
     let mut currentHandle = server;
     let mut poll = Poll::new().unwrap();
-    poll.registry().register(
-        &mut currentHandle,
-        SERVER,
-        Interest::WRITABLE 
-    ).unwrap();
+    poll.registry()
+        .register(&mut currentHandle, SERVER, Interest::WRITABLE)
+        .unwrap();
     let mut events = Events::with_capacity(128);
-   
+
     loop {
         match currentHandle.connect() {
-            Ok(()) =>{
+            Ok(()) => {
                 println!("Server Connected!");
-      
+
                 thread::spawn(move || {
                     let mut currentServer = currentHandle;
 
                     let mut count = 0;
                     loop {
-                        poll.registry().reregister(&mut currentServer, SERVER, Interest::READABLE).unwrap();
+                        poll.registry()
+                            .reregister(&mut currentServer, SERVER, Interest::READABLE)
+                            .unwrap();
                         poll.poll(&mut events, None).unwrap();
 
                         let mut buf = [0; 10];
                         match currentServer.read(&mut buf) {
                             Ok(0) => {
                                 continue;
-                            },
+                            }
                             Ok(_) => {
-                                print!("read: {:?}",  std::str::from_utf8(&buf));
-                            },
+                                print!("read: {:?}", std::str::from_utf8(&buf));
+                            }
                             Err(e) if e.kind() == std::io::ErrorKind::WouldBlock => {
                                 continue;
-                            },
+                            }
                             Err(e) => {
                                 println!("Error reading from pipe: {}", e);
                             }
                         }
 
-                        poll.registry().reregister(&mut currentServer, SERVER, Interest::WRITABLE).unwrap();
+                        poll.registry()
+                            .reregister(&mut currentServer, SERVER, Interest::WRITABLE)
+                            .unwrap();
 
                         count += 1;
                         loop {
-                            let message = format!("pong-{value}", value=count);
+                            let message = format!("pong-{value}", value = count);
                             match currentServer.write(message.as_bytes()) {
                                 Ok(_) => {
                                     println!("Wrote to pipe");
                                     break;
-                                },
+                                }
                                 Err(e) if e.kind() == std::io::ErrorKind::WouldBlock => {
                                     poll.poll(&mut events, None).unwrap();
-                                },
+                                }
                                 Err(e) => {
                                     println!("Error writing to pipe: {}", e);
-                                } 
+                                }
                             }
                         }
-                        
+
                         if count == 10 {
                             print!("killing client connection");
                             io::stdout().flush();
@@ -105,42 +103,38 @@ fn waitForConnection(server: NamedPipe) -> io::Result<NamedPipe> {
                             drop(currentServer);
                             break;
                         }
-                        
                     }
-                
+
                     print!("disconnecting");
                     io::stdout().flush();
                 });
 
                 // create a new one and loop for create again
                 return new(PIPE_NAME, false, std::ptr::null_mut());
-            },
+            }
             Err(e) if e.kind() == std::io::ErrorKind::WouldBlock => {
-                poll.registry().reregister(
-                    &mut currentHandle,
-                    SERVER,
-                    Interest::WRITABLE 
-                ).unwrap();
+                poll.registry()
+                    .reregister(&mut currentHandle, SERVER, Interest::WRITABLE)
+                    .unwrap();
 
-                poll.poll(&mut events,None).unwrap();
-            },
+                poll.poll(&mut events, None).unwrap();
+            }
             Err(e) => {
                 println!("Error connecting to pipe: {}", e);
-                return Err(e)
-            } 
+                return Err(e);
+            }
         }
     }
 }
 
-fn new<A: AsRef<OsStr>>(addr: A, first: bool, attrs: *mut c_void) -> io::Result<NamedPipe> {
+fn new<A: AsRef<OsStr>>(addr: A, first: bool, _attrs: *mut c_void) -> io::Result<NamedPipe> {
     let name: Vec<_> = addr.as_ref().encode_wide().chain(Some(0)).collect();
 
     // bitwise or file_flag_first_pipe_instance with file_flag_overlapped and pipe_access_duplex
-    let mut openmode = PIPE_ACCESS_DUPLEX |  FILE_FLAG_OVERLAPPED;
+    let mut openmode = PIPE_ACCESS_DUPLEX | FILE_FLAG_OVERLAPPED;
     if first {
         openmode |= FILE_FLAG_FIRST_PIPE_INSTANCE;
     }
-
 
     // Safety: syscall
     let h = unsafe {
